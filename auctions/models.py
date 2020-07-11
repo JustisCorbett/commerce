7bf36289053d3651e079c3a32ccd225c8e37e016
unknown <corbettjustis@gmail.com>
2020-07-10 18:35:30 -0600

    db models.py and first migration

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,39 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Categories(models.Model):
+    title = models.CharField(max_length=64)
+
+class Listings(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.IntegerField()
+    image = models.URLField()
+    category = models.ForeignKey(
+        Categories,
+        on_delete=models.CASCADE,
+    )
+
+class Bids(models.Model):
+    listing = models.ForeignKey(
+        Listings,
+        on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    amount = models.IntegerField()
+
+class Comments(models.Model):
+    listing = models.ForeignKey(
+        Listings,
+        on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    text = models.TextField()
+
